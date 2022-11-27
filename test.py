ffd1cc8bcf0fa9c8967e33bc95cac572eab554cd
MintqCodes <anhminh0822@gmail.com>
2022-11-27 21:24:15 +0700

    Update test.py
    Playing around with Tkinter

--- test.py
+++ test.py
@@ -4,4 +4,11 @@ random_list = []
 for i in range(0,6):
     n = random.randint(0, 9)
     random_list.append(n)
-    print(random_list)
+    print(random_list)#counterapp
+from tkinter import *
+root = Tk()
+myLabel1 = Label(root, text="Hello world!")
+myLabel2 = Label(root, text="I'm Minty!")
+myLabel1.grid(row=0,column=0)
+myLabel2.grid(row=1,column=0)
+root.mainloop()
